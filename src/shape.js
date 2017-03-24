5ff696bf668ffc5410d38b9c51e7c5745fc694dd
Colin Meinke <hello@colinmeinke.com>
2017-03-24 17:46:51 +0100

    feat(shape): return a shape

--- src/shape.js
+++ src/shape.js
@@ -1,13 +1,53 @@
+/**
+ * An SVG shape as defined by https://github.com/colinmeinke/svg-points.
+ *
+ * @typedef {Object} PlainShapeObjectCoreProps
+ */
+
+/**
+ * A three dimensional shape.
+ *
+ * @typedef {Object} PlainShapeObject
+ *
+ * @extends PlainShapeObjectCoreProps
+ */
+
+/**
+ * A four dimensional shape (i.e. includes time).
+ *
+ * @typedef {Object} Shape
+ *
+ * @property {PlainShapeObject[]} plainShapeObjects
+ */
+
 // @todo abstract this to the svg-points lib
 // and validate other properties
 const validate = plainShapeObjects => plainShapeObjects.map(s => {
   if (typeof s.type === 'undefined') {
-    throw new Error('type is a required property of a Plain Shape Object')
+    throw new TypeError('A Plain Shape Object must have a type property')
   }
 })
 
+/**
+ * Creates a Shape from one or more Plain Shape Object.
+ *
+ * @param {PlainShapeObject[]} plainShapeObjects
+ *
+ * @returns {Shape}
+ *
+ * @example
+ * shape(circle, square)
+ */
 const shape = (...plainShapeObjects) => {
+  if (plainShapeObjects.length === 0) {
+    throw new TypeError('The shape function must be passed at least one Plain Shape Object')
+  }
+
   validate(plainShapeObjects)
+
+  return {
+    plainShapeObjects
+  }
 }
 
 export default shape
--- tests/shape.js
+++ tests/shape.js
@@ -3,8 +3,26 @@
 import shape from '../src/shape'
 
 describe('shape', () => {
+  it('should throw if not passed a plain shape object', () => {
+    expect(() => shape())
+      .toThrow('The shape function must be passed at least one Plain Shape Object')
+  })
+
   it('should throw when passed an invalid plain shape object', () => {
     const invalidShape = { foo: 'bar' }
-    expect(() => shape(invalidShape)).toThrow()
+    expect(() => shape(invalidShape))
+      .toThrow('A Plain Shape Object must have a type property')
+  })
+
+  it('should return an object with the correct properties', () => {
+    const circle = {
+      type: 'circle',
+      cx: 50,
+      cy: 50,
+      r: 20,
+      fill: '#E54'
+    }
+
+    expect(shape(circle)).toHaveProperty('plainShapeObjects')
   })
 })
