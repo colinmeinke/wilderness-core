0bb2af6a2da83e8cd232164f7f3582418737cb0e
Colin Meinke <hello@colinmeinke.com>
2017-03-29 22:31:09 +0200

    feat: create entry point to export public api

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import frame from './frame'
+import keyframes from './keyframe'
+import plainShapeObject from './plain-shape-object'
+import shape from './shape'
+import timeline from './timeline'
+
+export {
+  frame,
+  keyframes,
+  plainShapeObject,
+  shape,
+  timeline
+}
