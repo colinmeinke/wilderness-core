0520860503ef81b3c9f50964922e523c4e3649b3
Colin Meinke <hello@colinmeinke.com>
2017-04-05 20:38:09 +0200

    feat(timeline): add outline of returned timeline object

--- src/timeline.js
+++ src/timeline.js
@@ -1,5 +1,39 @@
+const sort = props => {
+  return { shapes: props, options: {} }
+}
+
 const timeline = (...props) => {
-  return {}
+  const {
+    shapes,
+    options: {
+      alternate,
+      delay,
+      duration,
+      initialIterations,
+      iterations,
+      reverse,
+      started
+    }
+  } = sort(props)
+
+  return {
+    shapes: shapes.map(shape => ({
+      shape,
+      position: {
+        start: 0,
+        end: 1
+      }
+    })),
+    playback: {
+      alternate,
+      delay,
+      duration,
+      initialIterations,
+      iterations,
+      reverse,
+      started
+    }
+  }
 }
 
 export default timeline
