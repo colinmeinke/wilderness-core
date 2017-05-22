f8711259884d326fda62e4bbda73c62766217f75
Colin Meinke <hello@colinmeinke.com>
2017-05-22 17:50:02 +0100

    docs(examples): add playback options to playback example

--- examples/playback/src.js
+++ examples/playback/src.js
@@ -3,28 +3,65 @@ import { pause, shape, timeline } from '../../src'
 
 const playButton = document.querySelector('.play')
 const pauseButton = document.querySelector('.pause')
-const reverseButton = document.querySelector('.reverse')
+const initialIterationsInput = document.querySelector('#initialIterations')
+const iterationsInput = document.querySelector('#iterations')
+const alternateInput = document.querySelector('#alternate')
+const reverseInput = document.querySelector('#reverse')
+const durationInput = document.querySelector('#duration')
+
+const playbackOptions = () => {
+  const opts = {}
+
+  const initialIterations = initialIterationsInput.value.trim()
+  const iterations = iterationsInput.value.trim()
+  const alternate = alternateInput.value.trim()
+  const reverse = reverseInput.value.trim()
+  const duration = durationInput.value.trim()
+
+  if (!isNaN(parseFloat(initialIterations))) {
+    opts.initialIterations = parseFloat(initialIterations)
+  }
+
+  if (!isNaN(parseFloat(iterations))) {
+    opts.iterations = parseFloat(iterations)
+  } else if (iterations === 'Infinity') {
+    opts.iterations = Infinity
+  }
+
+  if (alternate === 'true' || alternate === 'false') {
+    opts.alternate = alternate === 'true'
+  }
+
+  if (reverse === 'true' || reverse === 'false') {
+    opts.reverse = reverse === 'true'
+  }
+
+  if (!isNaN(parseFloat(duration))) {
+    opts.duration = parseFloat(duration)
+  } else if (duration === 'Infinity') {
+    opts.duration = Infinity
+  }
+
+  return opts
+}
 
 const keyframe1 = { type: 'rect', width: 20, height: 20, x: 0, y: 40 }
 const keyframe2 = { type: 'rect', width: 20, height: 20, x: 80, y: 40 }
 
 const square = shape(keyframe1, keyframe2)
 
-const animation = timeline(square, {
-  alternate: true,
-  duration: 3000,
-  initialIteration: 0,
-  iterations: Infinity,
-  reverse: false
-})
+const animation = timeline(square)
 
 render(document.querySelector('svg'), animation)
 
-play(animation)
+playButton.addEventListener('click', () => {
+  const opts = playbackOptions()
+  play(animation, opts)
+  console.log('play', opts)
+})
 
-playButton.addEventListener('click', () => play(animation))
-pauseButton.addEventListener('click', () => pause(animation))
-reverseButton.addEventListener('click', () => {
-  console.log('reverse')
-  play(animation, { reverse: true })
+pauseButton.addEventListener('click', () => {
+  const opts = playbackOptions()
+  pause(animation, playbackOptions())
+  console.log('pause', opts)
 })
