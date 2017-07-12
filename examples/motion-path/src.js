fee50a26866705cecbca730f9e00dead857ab26d
Colin Meinke <hello@colinmeinke.com>
2017-07-12 16:57:25 -0500

    feat(motionPath): add rotate and group shape support
    Refs #6.

--- examples/motion-path/src.js
+++ examples/motion-path/src.js
@@ -1,31 +1,48 @@
 import { play, render } from '../crude-wilderness-dom'
 import { shape, motionPath, timeline } from '../../src'
 
-const circle = {
-  type: 'circle',
-  cx: 20,
-  cy: 20,
-  r: 20
+const arrows = {
+  type: 'g',
+  shapes: [
+    { type: 'path', d: 'M0,0L20,10L0,20Z' },
+    { type: 'path', d: 'M20,10L40,20L20,30Z' },
+    { type: 'path', d: 'M0,20L20,30L0,40Z' },
+  ]
 }
 
-const circleOnMotionPath = shape(circle, {
-  ...circle,
+const motionPathCircle = {
+  type: 'circle',
+  cx: 30,
+  cy: 30,
+  r: 30,
+}
+
+const arrowsOnMotionPath = shape(arrows, {
+  ...arrows,
   forces: [
     motionPath({
-      type: 'rect',
-      x: 0,
-      y: 0,
-      width: 60,
-      height: 60
+      ...motionPathCircle,
+      rotate: -90,
+      accuracy: 0.1
     })
   ]
 })
 
-const animation = timeline(circleOnMotionPath, {
+const animation = timeline(arrowsOnMotionPath, {
   duration: 3000,
   iterations: Infinity
 })
 
-render(document.querySelector('svg'), animation)
+render(
+  document.querySelector('svg'),
+  animation,
+  shape({
+    ...motionPathCircle,
+    fill: 'rgba(0,0,0,0.1)',
+    transforms: [
+      [ 'offset', 20, 20 ]
+    ]
+  })
+)
 
 play(animation)
--- src/motion-path-force.js
+++ src/motion-path-force.js
@@ -1,27 +1,54 @@
 import config from './config'
 import easingFunction from './easing-function'
-import { offset, position } from 'points'
+import { offset, position, rotate } from 'points'
 import { toPoints } from 'svg-points'
+
+const applyMotionPath = ({ angle, frameShape, x, y }) => {
+  if (frameShape.childFrameShapes) {
+    return {
+      ...frameShape,
+      childFrameShapes: frameShape.childFrameShapes.map(childFrameShape => (
+        applyMotionPath({ angle, frameShape: childFrameShape, x, y })
+      ))
+    }
+  }
+
+  const offsetPoints = offset(frameShape.points, x, y)
+  const points = angle ? rotate(offsetPoints, angle) : offsetPoints
+
+  return { ...frameShape, points }
+}
 
 const motionPath = ({
   accuracy = 1,
   easing: motionPathEasing = config.defaults.motionPath.easing,
-  rotate = false,
+  rotate: r = false,
   ...plainShapeObject
 }) => {
-  // @todo handle angle
+  // @todo apply transforms to motionPath shape
   // @todo add code comments
-  // @todo add validation
+  // @todo add validation (inc. error on group shape)
   // @todo add tests
 
-  const points = toPoints(plainShapeObject)
+  const motionPathPoints = toPoints(plainShapeObject)
   const easing = easingFunction(motionPathEasing)
 
   return (frameShape, framePosition) => {
     const motionPathPosition = easing(framePosition, 0, 1, 1)
-    const { angle, x, y } = position(points, motionPathPosition, accuracy)
+    const { angle, x, y } = position(motionPathPoints, motionPathPosition, accuracy)
 
-    return { ...frameShape, points: offset(frameShape.points, x, y) }
+    if (!x && !y) {
+      return frameShape
+    }
+
+    return applyMotionPath({
+      angle: typeof r === 'number'
+        ? (angle + r) % 360
+        : r === true ? angle : 0,
+      frameShape,
+      x,
+      y
+    })
   }
 }
 
