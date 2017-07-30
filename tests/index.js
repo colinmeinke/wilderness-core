c567b200c15ae7f58b511d0f6a0dd75b0053716b
Colin Meinke <hello@colinmeinke.com>
2017-07-30 15:40:15 -0500

    test: module exports

---
+++ tests/index.js
@@ -0,0 +1,80 @@
+/* globals describe it expect */
+
+import {
+  colorMiddleware,
+  events,
+  frame,
+  motionPath,
+  pause,
+  plainShapeObject,
+  play,
+  shape,
+  timeline,
+  unitMiddleware
+} from '../src'
+
+describe('colorMiddleware', () => {
+  it('should be exported', () => {
+    expect(typeof colorMiddleware).toBe('object')
+    expect(colorMiddleware).toHaveProperty('name')
+    expect(colorMiddleware).toHaveProperty('input')
+    expect(colorMiddleware).toHaveProperty('output')
+  })
+})
+
+describe('events', () => {
+  it('should be exported', () => {
+    expect(typeof events).toBe('function')
+  })
+})
+
+describe('frame', () => {
+  it('should be exported', () => {
+    expect(typeof frame).toBe('function')
+  })
+})
+
+describe('motionPath', () => {
+  it('should be exported', () => {
+    expect(typeof motionPath).toBe('function')
+  })
+})
+
+describe('pause', () => {
+  it('should be exported', () => {
+    expect(typeof pause).toBe('function')
+  })
+})
+
+describe('plainShapeObject', () => {
+  it('should be exported', () => {
+    expect(typeof plainShapeObject).toBe('function')
+  })
+})
+
+describe('play', () => {
+  it('should be exported', () => {
+    expect(typeof play).toBe('function')
+  })
+})
+
+describe('shape', () => {
+  it('should be exported', () => {
+    expect(typeof shape).toBe('function')
+  })
+})
+
+describe('timeline', () => {
+  it('should be exported', () => {
+    expect(typeof timeline).toBe('function')
+  })
+})
+
+describe('unitMiddleware', () => {
+  it('should be exported', () => {
+    expect(typeof unitMiddleware).toBe('object')
+    expect(unitMiddleware).toHaveProperty('name')
+    expect(unitMiddleware).toHaveProperty('input')
+    expect(unitMiddleware).toHaveProperty('output')
+  })
+})
